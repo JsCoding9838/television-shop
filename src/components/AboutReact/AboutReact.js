baec3f2f66de71031779c152101300a4e39b3b5d
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-03-27 00:32:44 +0800

    abount react updated

--- src/components/AboutReact/AboutReact.js
+++ src/components/AboutReact/AboutReact.js
@@ -5,28 +5,35 @@ const AboutReact = () => {
     return (
         <div className='react-container container'>
             <h2 className='mb-3 fw-bold'>Something About React</h2>
-            <div class="about-react row row-cols-1 row-cols-md-3 g-4">
-                <div class="col">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">How is React Works?</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. </p>
+            <div className="about-react row row-cols-1 row-cols-md-3 g-4">
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">How is React Works?</h5>
+                            <p className="card-text">
+                                React a javascript based UI development library. and it is widely used in web development. this is most commonly used frontend libraries for web development. and easy to understand the react library
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">Differences between Props vs State?</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. </p>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">Differences between Props vs State?</h5>
+                            <p className="card-text">
+                                State is a special property which is mutable property that hold a componet data. and it is default value when componet moutnts. generally, the difference is that state is monething like attributes in oop.
+                                and specialy that is the props are read only on the other hand State can be changes asynchronously.
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="col">
-                    <div class="card h-100">
-                        <div class="card-body">
-                            <h5 class="card-title">How is useState works?</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action. </p>
+                <div className="col">
+                    <div className="card h-100">
+                        <div className="card-body">
+                            <h5 className="card-title">How is useState works?</h5>
+                            <p className="card-text">
+                                The useState is a hook that allows have state variables in functional componets.react has two component one is class component which are ES6 that stands from react and the oter is functional component. The useState hook is special fuction that takes the initial state as an argument and return of the entries.
+                            </p>
                         </div>
                     </div>
                 </div>
